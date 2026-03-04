da18e90fc7330e57b8086e3b5ca58fab7a67ecda
MUHAMMAD ABBAS <malikabbaskhan477@gmail.com>
2026-03-04 21:16:19 +0500

    Add files via upload

---
+++ array_list/ARRAY_LIST_UPDATION.java
@@ -0,0 +1,17 @@
+package ARRAY_LIST;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ARRAY_LIST_UPDATION {
+
+	public static void main(String []args) {
+		
+		ArrayList obj=new ArrayList<>();
+		
+		obj.add("muhammad");
+		obj.add("malik");
+	System.out.println(obj);
+		
+		
+	}
+}
