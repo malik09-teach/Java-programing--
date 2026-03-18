c4700abeb3069a0945e06d93192fb2d6d918045e
MUHAMMAD ABBAS <malikabbaskhan477@gmail.com>
2026-03-18 19:40:04 +0500

    Add files via upload

---
+++ packages/importing_packs.java
@@ -0,0 +1,13 @@
+package packages;
+import LOOPS.DO_WHILE;
+
+public class importing_packs {
+	
+	public static void main (String []args) {
+		
+		
+	DO_WHILE obj=new DO_WHILE(); 
+		//directly imported other class public method to this packege 
+	System.out.println(obj.loopsystem());
+	
+}}
